79c60ba733609e65d3beed2ff00bdd4d81dd84a2
alessiademarco99 <76061803+alessiademarco99@users.noreply.github.com>
2023-04-05 14:15:24 +0300

    update

--- constraints.py
+++ constraints.py
@@ -5,16 +5,16 @@ class SphereConstraint:
         self.center = center
         self.r = r
         self.system = system
-  
-    def evaluate_constraint(self, x):	
+
+    def evaluate_constraint(self, x, k):	
 		#evaluate the state to see if the constraint is violated
         x=x.reshape(6*self.system.n_masses,1)
         x_next = self.system.transition(x, np.zeros(self.system.control_size))        
+
         length = (x_next[0] - self.center[0])**2 + (x_next[1] - self.center[1])**2 + (x_next[2] - self.center[2])**2
         return -(self.r**2 - length)
 
-
-    def evaluate_constraint_J(self, x):
+    def evaluate_constraint_J(self, x, k):
 		#evolve the system for one to evaluate constraint
         x=x.reshape(6*self.system.n_masses,1)
         x_next = self.system.transition(x, np.zeros(self.system.control_size))
@@ -26,6 +26,7 @@ class SphereConstraint:
         result[self.system.n_masses+1] = 2*(x_next[1] - self.center[1]) * self.system.dt
         result[self.system.n_masses+2] = 2*(x_next[2] - self.center[2]) * self.system.dt
         return np.ravel(result)
+        
 
 class BoxConstraint:
     def __init__(self,ux,lx,uy,ly,uz,lz,system, horizon):
@@ -50,17 +51,16 @@ class BoxConstraint:
                     if p[i,2] >= self.lz and p[i,2] <=self.uz:
                         return 1    
         return -1       
-       
-                                       
+      
+                        
+                    
     def evaluate_constraint_J(self, x):
         x=x.reshape(6*self.system.n_masses,1)
-        #if k not in range(self.horizon-10,self.horizon+1):
+
         result = -np.ones(x.shape) * self.system.dt
         for i in range(self.system.n_masses*3):
             result[i]= -1
         return np.ravel(result)
-        # else:
-        #     result = np.zeros(x.shape)
-        #     return np.ravel(result)
+        
         
   
