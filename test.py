6ca19d50e76ee84fcd25292826a08633f4655e2b
alessiademarco99 <76061803+alessiademarco99@users.noreply.github.com>
2023-04-05 14:36:48 +0300

    cost function
    final cost matrix gives higher weights for the last three masses

--- test.py
+++ test.py
@@ -6,33 +6,35 @@ from AL_iLQR import AL_iLQR
 from constraints import SphereConstraint,BoxConstraint
 import matplotlib.pyplot as plt 
 if __name__ == '__main__':
-    system=Rope(5,0.001) 
+    system=Rope(5,0.005) 
     fig = plt.figure(figsize=(8, 6), dpi=100) 
     ax = fig.add_subplot(111, projection='3d')
-    x0=system.initial_position(0,0,4)
+    x0=system.initial_position(-2,0,6)
     system.plot_rope(ax,x0)
     #print(np.ones((1,601)).shape)
-    system.set_cost(1e-4*np.identity(system.state_size), 0.005*np.identity(system.control_size))
-    Q_f = 1e-4*np.identity(system.state_size)
+    system.set_cost(0*np.identity(system.state_size), 0.05*np.identity(system.control_size))
+    Q_f = 10*np.identity(system.state_size)
     for i in range(3*system.n_masses):
-        if i >= (3*system.n_masses - 6):
-            Q_f[i, i] = 150 #more important the last ones 
+        Q_f[i, i] = 150*i #more important the last ones
     system.set_final_cost(Q_f)
     
     system.set_goal(np.ravel(system.goal_position(1,0,4.2)))
     print(system.goal_position(1,0,4.2))
     fig = plt.figure(figsize=(8, 6), dpi=100)
     ax = fig.add_subplot(111, projection='3d')
-    system.plot_rope(ax,system.goal_position(2,0,3.2))
+    system.plot_rope(ax,system.goal_position(1,0,4.2))
     #print(system.initial_position(0,0,0))
-    horizon=2000
+    horizon=500
     
     constraint1=SphereConstraint(system.return_position_actuated_mass(x0), 5, system)
     constraint2=BoxConstraint(6.5,3.5,1,-1,3.2,2.0,system,horizon)
-    
+    #constraint3=CircleConstraint(np.array([5, 0]), 0.5, system, 9)
+    #constraint2=BoxSurface_xy(0,1,4, system, 9,1)
+    #constraint3=BoxSurface_xy(0,1,4, system, 9,0)
     solver=AL_iLQR(system, x0, horizon)
     solver.add_constraint(constraint1)
     solver.add_constraint(constraint2)
+    #solver.add_constraint(constraint3)
     solver.algorithm()
     fig = plt.figure(figsize=(8, 6), dpi=100)
     ax = fig.add_subplot(111, projection='3d')
