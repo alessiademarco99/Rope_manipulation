7006a2f3fc68fd09c3f3d0e087f33f5740558441
alessiademarco99 <76061803+alessiademarco99@users.noreply.github.com>
2023-04-05 14:05:57 +0300

    update

--- iLQR.py
+++ iLQR.py
@@ -14,7 +14,7 @@ class LQR:
        
         self.penalty=10
         self.alpha=1
-        self.e_constraint=0.1
+        self.e_constraint=0.5
         self.d=np.zeros((self.system.control_size, self.horizon))
         self.K=np.zeros((self.system.control_size, self.system.state_size, self.horizon))
         self.delta_V1=np.zeros(self.horizon)
@@ -25,7 +25,7 @@ class LQR:
         self.x_traj = np.copy(x_traj)
         self.u_traj = np.copy(u_traj)
         
-    #provare a non modificare self.best_J ma argomenti e returns
+    
     def forward_pass(self,x_up,u_up,J_prev):
         done=0
         new_J=0
@@ -44,9 +44,7 @@ class LQR:
                 u_new_traj[:, i] = np.copy(np.ravel(u))
                 new_J += self.system.calculate_cost(x, u)
                 x = self.system.transition(x, u)
-                #print("Forward pass")
-            
-            # print(new_J.shape)    
+         
             x_new_traj[:, self.horizon] = np.copy(np.ravel(x))
             new_J += self.system.calculate_final_cost(x)
             
@@ -56,7 +54,7 @@ class LQR:
             for i in range(self.horizon):
                 J+= self.system.calculate_cost(x_up[:,i], u_up[:,i])
             J+= self.system.calculate_final_cost(x_up[:,self.horizon])
-            #z=(current_J-new_J)/(-np.sum(adelta_V)) #problema
+            
             z=(J-new_J)/(-np.sum(adelta_V))
             print("z ",z)
             print("J_prev ",J)
@@ -66,7 +64,7 @@ class LQR:
             if iter == self.max_iter:
                 print('max iter')
                 return x_up,u_up, J_prev
-            if (z < 30 and z > 1e-8): #or iter==1: #line search condition  z < 10 and 
+            if (z < 15 and z > 1e-8): #or iter==1: #line search condition  
                 x_up=np.copy(x_new_traj)
                 u_up=np.copy(u_new_traj)
                 
@@ -97,13 +95,13 @@ class LQR:
         C=np.ones(len(self.constraints))
         C_x=np.zeros((len(self.constraints),self.system.state_size))
         for i in range(len(self.constraints)):
-            C[i]=self.constraints[i].evaluate_constraint(x_new[:, self.horizon], self.horizon)
+            C[i]=self.constraints[i].evaluate_constraint(x_new[:, self.horizon])
             
         # checking if the constraint is active
             if C[i]  < - self.e_constraint and self.multipliers[i,self.horizon-1] == 0: 
                 self.Iu[i,i] = 0 # it means that the constraint is not active
             
-            C_x[i,:]=self.constraints[i].evaluate_constraint_J(x_new[:, self.horizon], self.horizon)
+            C_x[i,:]=self.constraints[i].evaluate_constraint_J(x_new[:, self.horizon])
         cu=np.zeros((len(self.constraints),self.system.control_size)) # no constraints on control trajectory
         pn=ln_x+C_x.T @ (self.multipliers[:,self.horizon-1]+self.Iu @ C)
         Pn=ln_xx+C_x.T @ self.Iu @ C_x
@@ -120,8 +118,8 @@ class LQR:
             l_uut = np.copy(self.system.R)
             self.Iu=self.mu[:,i]*np.diag(np.ones(len(self.constraints)))
             for j in range(len(self.constraints)):
-                C_x[j,:]=self.constraints[j].evaluate_constraint_J(x,i)
-                C[j]=self.constraints[j].evaluate_constraint(x,i)
+                C_x[j,:]=self.constraints[j].evaluate_constraint_J(x)
+                C[j]=self.constraints[j].evaluate_constraint(x)
                 if C[j]  < - self.e_constraint and self.multipliers[j,i] == 0:
                     self.Iu[j,j] = 0
             if i+1==self.horizon:
@@ -130,7 +128,7 @@ class LQR:
             else:
                 p=Q_x + self.K[:,:,i+1].T @ Q_uu @ self.d[:,i+1] + self.K[:,:,i+1].T @ Q_u + Q_ux.T @ self.d[:,i+1]
                 P=Q_xx + self.K[:,:,i+1].T @ Q_uu @ self.K[:,:,i+1]+ self.K[:,:,i+1].T @ Q_ux + Q_ux.T @ self.K[:,:,i+1]
-            # print("I'm here") 
+            
             A, B = self.system.transition_J(x,u)  #matrices A and B from dynamics
             Q_x = l_xt + A.T @ p + C_x.T @ (self.multipliers[:,i]+self.Iu @ C)
             Q_u = l_ut + B.T @ p + cu.T @ (self.multipliers[:,i]+self.Iu @ C)
