2d7d09ef90fa72f35adfb80bd1aa7664ed74110a
alessiademarco99 <76061803+alessiademarco99@users.noreply.github.com>
2023-04-05 14:42:05 +0300

    update

--- test.py
+++ test.py
@@ -6,35 +6,33 @@ from AL_iLQR import AL_iLQR
 from constraints import SphereConstraint,BoxConstraint
 import matplotlib.pyplot as plt 
 if __name__ == '__main__':
-    system=Rope(5,0.005) 
+    system=Rope(5,0.001) 
     fig = plt.figure(figsize=(8, 6), dpi=100) 
     ax = fig.add_subplot(111, projection='3d')
-    x0=system.initial_position(-2,0,6)
+    x0=system.initial_position(0,0,4)
     system.plot_rope(ax,x0)
     #print(np.ones((1,601)).shape)
-    system.set_cost(0*np.identity(system.state_size), 0.05*np.identity(system.control_size))
-    Q_f = 10*np.identity(system.state_size)
+    system.set_cost(1e-4*np.identity(system.state_size), 0.005*np.identity(system.control_size))
+    Q_f = 1e-4*np.identity(system.state_size)
     for i in range(3*system.n_masses):
-        Q_f[i, i] = 150*i #more important the last ones
+        if i >= (3*system.n_masses - 9):
+            Q_f[i, i] = 150 #more important the last ones 
     system.set_final_cost(Q_f)
     
     system.set_goal(np.ravel(system.goal_position(1,0,4.2)))
     print(system.goal_position(1,0,4.2))
     fig = plt.figure(figsize=(8, 6), dpi=100)
     ax = fig.add_subplot(111, projection='3d')
-    system.plot_rope(ax,system.goal_position(1,0,4.2))
+    system.plot_rope(ax,system.goal_position(2,0,3.2))
     #print(system.initial_position(0,0,0))
-    horizon=500
+    horizon=2000
     
     constraint1=SphereConstraint(system.return_position_actuated_mass(x0), 5, system)
     constraint2=BoxConstraint(6.5,3.5,1,-1,3.2,2.0,system,horizon)
-    #constraint3=CircleConstraint(np.array([5, 0]), 0.5, system, 9)
-    #constraint2=BoxSurface_xy(0,1,4, system, 9,1)
-    #constraint3=BoxSurface_xy(0,1,4, system, 9,0)
+    
     solver=AL_iLQR(system, x0, horizon)
     solver.add_constraint(constraint1)
     solver.add_constraint(constraint2)
-    #solver.add_constraint(constraint3)
     solver.algorithm()
     fig = plt.figure(figsize=(8, 6), dpi=100)
     ax = fig.add_subplot(111, projection='3d')
